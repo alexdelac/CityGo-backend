f96ce092f3a10f278d8424114b1a3b00774dc0fc
Alexandre Delacre <delacre.alex@gmail.com>
2023-12-08 16:23:03 +0100

    ajout séparation pour nos tests

--- app.test.js
+++ app.test.js
@@ -2,3 +2,60 @@ const request = require('supertest');
 const app = require('./app');
 
 
+// test Marie (start ligne 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//test camille (start ligne 31)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//test alex (start ligne 60)
+
